e12e379aa4ec3d934f66e2e8d836cc42bf605ef9
AhmedSherif2002 <113216034+AhmedSherif2002@users.noreply.github.com>
2024-10-23 19:28:05 +0300

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT | 3300;
+const port = process.env.PORT | 5000;
 
 app.listen(port, (req,res)=> console.log("Server is listening on port", port));
 
