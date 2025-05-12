89af7ae47c21a0cf51d1a7394ac39462d066d7ff
Sameh Ossama <43967303+SamehOssama@users.noreply.github.com>
2025-05-12 21:22:35 +0300

    18

--- app.js
+++ app.js
@@ -7,5 +7,5 @@ const port = process.env.PORT | 5000;
 app.listen(port, (req,res)=> console.log("Server is listening on port", port));
 
 app.get("",(req,res)=>{
-    res.send("17");
+    res.send("18");
 })
