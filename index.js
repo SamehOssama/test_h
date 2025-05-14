86c878cc719afc0c1c557b38cd46e5072b56858a
Sameh Ossama <43967303+SamehOssama@users.noreply.github.com>
2025-05-14 15:43:26 +0300

    feat: take number from env or default

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT | 5000;
+const port = process.env.PORT?? 5000;
+const num = process.env.NUM?? "22";
 
 app.listen(port, (req,res)=> console.log("Server is listening on port", port));
 
-app.get("",(req,res)=>{
-    res.send("22");
+app.get("", (req,res)=>{
+    res.send(num);
 })
