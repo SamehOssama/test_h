945322f68c66c8bb71095c6592414204baccf5f1
Sameh Ossama <43967303+SamehOssama@users.noreply.github.com>
2025-05-14 20:12:09 +0300

    25

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 const port = process.env.PORT?? 5000;
-const num = process.env.NUM?? "24";
+const num = process.env.NUM?? "25";
 
 app.listen(port, (req,res)=> console.log("Server is listening on port", port));
 
